445e2e150bb83412d81489d0088c7e7967f14e4e
Timothy Kim <159036101+TheTimKim45@users.noreply.github.com>
2025-06-11 20:06:38 -0400

    Optimization 1

--- MadeScore.cpp
+++ MadeScore.cpp
@@ -4,15 +4,35 @@ namespace MS
 {
 	MadeScore::ScoreUnit::ScoreUnit()
 	{
-		grade = 0;
+		/*grade = 0;
 		weight = 3;
-
+		notes = "";*/
 	}
 
 	MadeScore::MadeScore()
 	{
 		//Initial Game Score
 		GameGrade = 0;
+		GradeM = ScoreUnit({
+			{M_C1, ScoreUnit()},
+			{M_C2, ScoreUnit()},
+			{M_C3, ScoreUnit()}
+			});
+		GradeA = ScoreUnit({
+			{A_C1, ScoreUnit()},
+			{A_C2, ScoreUnit()},
+			{A_C3, ScoreUnit()}
+			});
+		GradeD = ScoreUnit({
+			{D_C1, ScoreUnit()},
+			{D_C2, ScoreUnit()},
+			{D_C3, ScoreUnit()}
+			});
+		GradeE = ScoreUnit({
+			{E_C1, ScoreUnit()},
+			{E_C2, ScoreUnit()},
+			{E_C3, ScoreUnit()}
+			});
 	}
 
 	void MadeScore::CalculateFinalGrade()
@@ -26,17 +46,17 @@ namespace MS
 
 	void MadeScore::Set_DIV_Grade(ScoreUnit& c1, ScoreUnit& c2, ScoreUnit& c3, ScoreUnit& divisionUnit)
 	{
-		float calcGrade = static_cast<float>(c1.weight * (c1.grade + 5));
-		calcGrade += static_cast<float>(c2.weight * (c2.grade + 5));
-		calcGrade += static_cast<float>(c3.weight * (c3.grade + 5));
+		float calcGrade = static_cast<float>(c1.weight * (c1.grade + CAT_MUL1));
+		calcGrade += static_cast<float>(c2.weight * (c2.grade + CAT_MUL1));
+		calcGrade += static_cast<float>(c3.weight * (c3.grade + CAT_MUL1));
 		calcGrade /= static_cast<float>(c1.weight + c2.weight + c3.weight);
-		calcGrade *= 10.f;
+		calcGrade *= CAT_MUL2;
 		divisionUnit.grade = static_cast<int>(calcGrade + 0.5f);
 	}
 
 	MadeScore::ResCode MadeScore::Set_CAT_Grade(const int newGrade, ScoreUnit& scoreUnit)
 	{
-		if (newGrade >= -5 && newGrade <= 5)
+		if (newGrade >= MIN_GRADE && newGrade <= MAX_GRADE)
 		{
 			scoreUnit.grade = newGrade;
 			return SUCCESS;
@@ -47,7 +67,7 @@ namespace MS
 
 	MadeScore::ResCode MadeScore::Set_CAT_Weight(const int newWeight, ScoreUnit& scoreUnit)
 	{
-		if (newWeight >= 0 && newWeight <= 5)
+		if (newWeight >= MIN_WEIGHT && newWeight <= MAX_WEIGHT)
 		{
 			scoreUnit.weight = newWeight;
 			return SUCCESS;
@@ -66,7 +86,7 @@ namespace MS
 
 	MadeScore::ResCode MadeScore::Set_DIV_Weight(const int newWeight, ScoreUnit& divisionUnit)
 	{
-		if (newWeight >= 0 && newWeight <= 5)
+		if (newWeight >= MIN_WEIGHT && newWeight <= MAX_WEIGHT)
 		{
 			divisionUnit.weight = newWeight;
 			return SUCCESS;
@@ -74,11 +94,12 @@ namespace MS
 		else
 			return ERROR;
 	}
+
 	void MadeScore::Print_Menu()
 	{
-		printf("%s\n", MENU_TITLE);
+		/*printf("%s\n", MENU_TITLE);
 		printf("%s\n", MSG_01);
 		printf("\n%s\n", SEL_ONE);
-		printf("%s\n", SEL_TWO);
+		printf("%s\n", SEL_TWO);*/
 	}
 }
--- MadeScore.h
+++ MadeScore.h
@@ -1,11 +1,31 @@
 #pragma once
 #include <iostream>
-
+#include <unordered_map>
 //precompiler MACROS
-#define MENU_TITLE "===MADE REVIEW SYSTEMS==="
-#define SEL_ONE "01)--------REVIEWER"
-#define SEL_TWO "02)--------USER"
-#define MSG_01 "PLEASE SELECT ROLE:"
+//MAIN MACROS
+#define MIN_WEIGHT	0
+#define MAX_WEIGHT	5
+#define MIN_GRADE	-5
+#define MAX_GRADE	5
+//SUB-DIV CATS
+#define M_C1	"actions"
+#define M_C2	"controls"
+#define M_C3	"environment"
+
+#define A_C1	"writing"
+#define A_C2	"audio"
+#define A_C3	"visuals"
+
+#define D_C1	"performance"
+#define D_C2	"accessibility"
+#define D_C3	"experience"
+
+#define E_C1	"barriers"
+#define E_C2	"extramedia content"
+#define E_C3	"ethics"
+
+#define CAT_MUL1	5
+#define CAT_MUL2	10.f
 namespace MS
 {
 	class MadeScore
@@ -16,10 +36,12 @@ namespace MS
 		
 		struct ScoreUnit
 		{
-			int grade;
-			int weight;
-			std::string notes;
+			std::unordered_map < std::string, ScoreUnit> CAT;
+			int grade = 0;
+			int weight = 3;
+			std::string notes = "";
 			ScoreUnit();
+			ScoreUnit(std::initializer_list < std::pair < std::string, ScoreUnit>> items) { for (auto& p : items) CAT[p.first] = p.second; };
 		};
 
 		enum ResCode
@@ -31,11 +53,10 @@ namespace MS
 		//Divisions
 		ScoreUnit GradeM, GradeA, GradeD, GradeE;
 		//Categories
-		ScoreUnit m_actions, m_controls, m_environment;
+		/*ScoreUnit m_actions, m_controls, m_environment;
 		ScoreUnit a_writing, a_audio, a_visuals;
 		ScoreUnit d_performance, d_accessibility, d_experience;
-		ScoreUnit e_barriers, e_extramediacontent, e_ethics;
-
+		ScoreUnit e_barriers, e_extramediacontent, e_ethics;*/
 
 	public:
 		MadeScore();
@@ -47,25 +68,25 @@ namespace MS
 		ResCode Set_CAT_Note(const std::string newNote, ScoreUnit& scoreUnit);
 		ResCode Set_DIV_Weight(const int newWeight, ScoreUnit& divisionUnit);
 		//Accessors
-		ScoreUnit& Get_M_C1() { return m_actions; } 
-		ScoreUnit& Get_M_C2() { return m_controls; } 
-		ScoreUnit& Get_M_C3() { return m_environment; } 
 		ScoreUnit& Get_M() { return GradeM; } 
+		ScoreUnit& Get_M_C1() { return GradeM.CAT[M_C1]; }
+		ScoreUnit& Get_M_C2() { return GradeM.CAT[M_C2]; }
+		ScoreUnit& Get_M_C3() { return GradeM.CAT[M_C3]; }
 
-		ScoreUnit& Get_A_C1() { return a_writing; } 
-		ScoreUnit& Get_A_C2() { return a_audio; } 
-		ScoreUnit& Get_A_C3() { return a_visuals; } 
-		ScoreUnit& Get_A() { return GradeA; } 
+		ScoreUnit& Get_A() { return GradeA; }
+		ScoreUnit& Get_A_C1() { return GradeA.CAT[A_C1]; }
+		ScoreUnit& Get_A_C2() { return GradeA.CAT[A_C2]; }
+		ScoreUnit& Get_A_C3() { return GradeA.CAT[A_C3]; }
 
-		ScoreUnit& Get_D_C1() { return d_performance; } 
-		ScoreUnit& Get_D_C2() { return d_accessibility; } 
-		ScoreUnit& Get_D_C3() { return d_experience; } 
-		ScoreUnit& Get_D() { return GradeD; } 
+		ScoreUnit& Get_D() { return GradeD; }
+		ScoreUnit& Get_D_C1() { return GradeD.CAT[D_C1]; }
+		ScoreUnit& Get_D_C2() { return GradeD.CAT[D_C2]; }
+		ScoreUnit& Get_D_C3() { return GradeD.CAT[D_C3]; }
 
-		ScoreUnit& Get_E_C1() { return e_barriers; } 
-		ScoreUnit& Get_E_C2() { return e_extramediacontent; } 
-		ScoreUnit& Get_E_C3() { return e_ethics; } 
-		ScoreUnit& Get_E() { return GradeE; } 
+		ScoreUnit& Get_E() { return GradeE; }
+		ScoreUnit& Get_E_C1() { return GradeE.CAT[E_C1]; }
+		ScoreUnit& Get_E_C2() { return GradeE.CAT[E_C2]; }
+		ScoreUnit& Get_E_C3() { return GradeE.CAT[E_C3]; }
 		//Displayers - printf stuff
 		void Print_Menu();
 		//Menu Simulation
